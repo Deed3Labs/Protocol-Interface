85094118f5949600510a820d16cd29482099d2d8
Isaiah "KyngKai" Litt <kyngkai909@gmail.com>
2025-03-26 12:46:40 -0700

    Update Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,11 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-background dark:bg-[#0e0e0e]">
       <Navbar />
-      {children}
+      <main className="container mx-auto px-4">
+        {children}
+      </main>
     </div>
   );
 };
