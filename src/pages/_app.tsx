c27e35198e4e242d19e5dd2443509c36cb3059ce
Isaiah "KyngKai" Litt <kyngkai909@gmail.com>
2025-03-26 12:42:39 -0700

    Update theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider, createConfig, http } from 'wagmi'
+import { createConfig, WagmiProvider, http } from 'wagmi'
 import { sepolia } from 'wagmi/chains'
-import { injected } from 'wagmi/connectors'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from '@/components/theme-provider'
 import Layout from '@/components/Layout'
+import { injected } from 'wagmi/connectors'
 
 const queryClient = new QueryClient()
 
 const config = createConfig({
   chains: [sepolia],
-  connectors: [injected()],
+  connectors: [
+    injected(),
+  ],
   transports: {
     [sepolia.id]: http(),
   },
@@ -19,19 +21,19 @@ const config = createConfig({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Layout>
             <Component {...pageProps} />
           </Layout>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </ThemeProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
